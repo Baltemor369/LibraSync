c72f9aa2e4b8e57f407518466023cf2d0865ffa8
Rikudo4485 <thomespo@gmail.com>
2023-03-15 02:46:48 +0100

    new featuren: volume number can be written '1,4-6,8'and it's equal to '1,4,5,6,8'

---
+++ Manga_Library1.1/main.py
@@ -0,0 +1,15 @@
+from modules.MangaUI import UI
+import os
+
+# To do :
+# + volume "1-4"=> du volume 1 au 4
+# + faire alignement affichage statistique
+# + développer module historique(moyenne par semaine/mois voir combien ont été ajout pendant une semaine donnée)
+# + fichier backup
+
+if not os.path.exists("data"):
+    os.mkdir("data")
+    os.system("type nul > data\data.txt")
+
+ui = UI()
+ui.mainloop()
