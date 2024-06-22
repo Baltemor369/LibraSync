fa0b92089d5889e7f17239a9a237be2f30ca1ced
Rikudo4485 <thomespo@gmail.com>
2024-06-22 21:20:23 +0200

    run upgrade

--- run.bat
+++ run.bat
@@ -1,27 +1,20 @@
 @echo off
 
-IF "%~1"=="-help" GOTO helper
-
 IF NOT EXIST .env (
+    echo environment creation..
     python -m venv .env
     cd .env\Scripts
+    echo environment activation
     call activate.bat
+    echo environment activate
     cd ../..
     pip install -r requirements.txt
 ) ELSE (
     cd .env\Scripts
+    echo environment activation
     call activate.bat
+    echo environment activate
     cd ../..
 )
-python.exe main.py
-exit /b 0
-
-:helper
-echo NOM 
-echo    envPython : create and start a virtual env for Python project. 
-echo.
-echo SYNTAXE
-echo    envPython [-OPTION]
-echo.
-echo OPTION
-echo    -h : the helper.
+echo lauch app
+python main.py
