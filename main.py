cb410ffcfcd6adadafb673c65b2ac03fa0246eea
Baltemor369 <thomespo@gmail.com>
2024-07-10 18:40:30 +0200

    loading data from gDrive is working

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
-from googleapiclient.http import MediaFileUpload, MediaIoBaseDownload
+from googleapiclient.http import MediaIoBaseDownload, MediaFileUpload
 
 SCOPES = ["https://www.googleapis.com/auth/drive"]
 creds = None
@@ -26,43 +26,51 @@ def authentification():
 
                 with open(TOKEN, "w") as token:
                     token.write(creds.to_json())
+                
             except Exception as error:
                 print(f"An error occurred: {error}")
 
     return creds.valid if creds else False
 
 
-def  load(creds):
-    # build the object to interact with Google Drive API
-    service = build("drive", "v3", credentials=creds)
-    # get the list of files named 'data.db'
-    results = service.files().list(q="name='"+DATA_FILE+"'").execute()
-    items = results.get("files", [])
+def load(creds):
+    try:
+        # build the object to interact with Google Drive API
+        service = build("drive", "v3", credentials=creds)
 
-    # if no file named 'data.db' is found, create it
-    if not items:
-        file_metadata = {"name": DATA_FILE}
-        media = MediaFileUpload(DATA_FILE, mimetype="application/octet-stream")
-        service.files().create(body=file_metadata, media_body=media).execute()
-        print(f"File {DATA_FILE} created successfully")
-    # if the file named 'data.db' is found, load its content
-    else:
-        file_id = items[0]["id"]
+        # get the list of files named 'data.db' in the root directory of the Google Drive account
+        query = f"name='{DATA_FILE}'"
+        results = service.files().list(q=query, fields='files(id, name)').execute()
+        items = results.get('files', [])
+
+        if not items:
+            print(f"File {DATA_FILE} not found. Creation...")
+            file_metadata = {"name": DATA_FILE}
+            media = MediaFileUpload(DATA_FILE, mimetype="application/octet-stream")
+            service.files().create(body=file_metadata, media_body=media).execute()
+            print(f"File {DATA_FILE} created successfully")
+            
+            # get the id of the newly created file
+            results = service.files().list(q=query, fields='files(id, name)').execute()
+            items = results.get('files', [])
+            
+        file_id = items[0]['id']
         request = service.files().get_media(fileId=file_id)
+
         with open(DATA_FILE, "wb") as data_file:
             downloader = MediaIoBaseDownload(data_file, request)
             done = False
             while done is False:
                 status, done = downloader.next_chunk()
-
-        # with open(DATA_FILE, "wb") as data_file:
-        #     data = json.dumps(request.execute())
-        #     data_file.write(data.encode())
         print(f"File {DATA_FILE} loaded successfully")
-
+        return True
+    except Exception as e:
+        print(f"A error occured : {e}")
+        return False
 
 if __name__ == "__main__":
     if authentification():
-        print(f"authentification successful")
-        load(creds)
-        # Interface()
+        print(f"Google authentification successful")
+        if load(creds):
+            Interface()
+            pass
