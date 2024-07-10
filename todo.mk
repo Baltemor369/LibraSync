38f83c264d674be153f5e73e33c3600af7ce433d
Baltemor369 <thomespo@gmail.com>
2024-07-10 20:06:46 +0200

    add a todo list

---
+++ todo.mk
@@ -0,0 +1,6 @@
+
+1 extend input width in add_new_book
+2 find explanation of the issue with families
+3 resolve the issue with families 
+4 save data.db on the drive when exiting
+5 add a button to save data.db on the drive
