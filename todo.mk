7434eb3df0c6f0b63bb151b2136a7e0fb663b9d2
Baltemor369 <thomespo@gmail.com>
2024-07-16 01:02:29 +0200

    todo update

--- todo.mk
+++ todo.mk
@@ -1,5 +1,3 @@
-2 find explanation of the issue with families
-3 resolve the issue with families 
 5 add a button to save data.db on the drive
 6 button "start" "end"
 7 bind scrolling to next/previous page
